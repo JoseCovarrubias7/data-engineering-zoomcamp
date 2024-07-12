2c1376f1168afe721983ff1455ec895b5973ef92
Jose Alberto Covarrubias Flores <jose.covarrubias.fl@gmail.com>
2024-07-12 19:42:20 +0000

    update

--- 04-analytics-engineering /taxi_rides_ny/models/stg_yellow_tripdata.sql
+++ 04-analytics-engineering /taxi_rides_ny/models/stg_yellow_tripdata.sql
@@ -8,7 +8,7 @@ with tripdata as
 (
   select *,
     row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
-  from {{ source('staging','green_tripdata') }}
+  from {{ source('staging','yellow_tripdata') }}
   where vendorid is not null 
 )
 select
